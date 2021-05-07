0edd8276e5f89ccbe6b0e125bff31fb6562adef0
KCSoftDL <78419406@qq.com>
2021-05-07 11:58:38 +0800

    添加一些小脚本，如读txt文件、删除不需要文件等功能，同时更新可视化窗口

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import pandas as pd
 import tensorflow as tf
 from tflearn.data_utils import build_hdf5_image_dataset
 
@@ -16,6 +17,49 @@ def read_from_dir_and_write(path):
             obj = obj + '\n'
             f.write(obj)
     print('finish write,thanks!')
+
+def read_and_rewrite(path):
+    '''
+    处理useless.txt文本中的前缀，并写下处理结果
+    :param path:
+    :return:
+    '''
+    a = "F:"
+    b = "useless"
+    with open(path,'r+',encoding='utf-8') as f:
+        filenames = f.readlines()
+        for filename in filenames:
+            print(filename)
+            # filename.replace(b,"",1)
+            # print(filename)
+            list_str = list(filename)
+            for i in range(11):
+                list_str.pop(0)
+            filename =''.join(list_str)
+            print(filename)
+            f.writelines(filename)
+        # filename.lstrip(b)
+        # print(filename)
+
+def remove_useless_img(imgpath,txtpath):
+    '''
+    删除imgpath路径下,txtpath文件中记录的内容
+    :param imgpath: 图像路径
+    :param txtpath: 文本路径
+    :return:
+    '''
+    with open(txtpath, 'r+', encoding='utf-8') as f:
+        filenames = f.readlines()
+        for filename in filenames:
+            filename = filename.rstrip('\n')
+            # print(filename)
+            img = os.path.join(imgpath,filename)
+            print("now filename is {}".format(img))
+            if os.path.exists(img):
+                print("remove {}".format(img))
+                os.remove(img)
+            else:print("文件不存在，pass!")
+    print("remove all right!")
 
 def write_txt_from_dir(path,name,target_size):
     train_image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255,  # 归一化
@@ -38,14 +82,47 @@ def write_txt_from_dir(path,name,target_size):
             f.write(obj)
     return textpath
 
+def create_VOC_data_txt(path,txt_path):
+    lables = os.listdir(path)
+    with open(txt_path,'w+', encoding='utf-8') as f:
+        for lable in lables:
+            image = os.listdir(path + "/"+ lable)
+            for i in range(len(image)):
+                image[i] = lable + "/" + image[i]
+                f.write(image[i]+'\n')
+
+
 def rewrite_data(train_path,val_path):
     build_hdf5_image_dataset(train_path, image_shape=(488, 488), mode='file', output_path='new_train_488.h5',
                              categorical_labels=True, normalize=False)
     build_hdf5_image_dataset(val_path, image_shape=(224, 224), mode='file', output_path='new_val_224.h5',
                              categorical_labels=True, normalize=False)
 
+def read_chinesefoodnet_from_xlsx(path):
+    '''
+    读取excle的文件前三列
+    :param path: the root of .xls
+    :return: 3 list : id,ChineseName,EnglishName
+    '''
+    path = os.path.join(path,"class_names.xls")
+    # print(path)
+    cols0 = pd.read_excel(path,sheet_name='Sheet1',usecols=[0]).values
+    cols1 = pd.read_excel(path, sheet_name='Sheet1', usecols=[1]).values
+    cols2 = pd.read_excel(path, sheet_name='Sheet1', usecols=[2]).values
+    # print("{}-->{}/{}".format(cols0[0],cols1[0],cols2[0]))
+    # print(cols1[000][0])
+    return cols0,cols1,cols2
+
 if __name__ == "__main__":
-    path = "D:\Programming/tensorflow\data/train"
-    train_path = write_txt_from_dir(path,'train',target_size=448)
-    val_path = write_txt_from_dir(path,'val',target_size=224)
+    path = "D:\BaiduNetdiskDownload\dataset_release/release_data"
+    txt_path = path + "/trainval.txt"
+    text_path ="D:\健康拍立得\datasets\ChineseFoodNet/useless.txt"
+    # train_path = write_txt_from_dir(path + '/train','train',target_size=448)
+    # val_path = write_txt_from_dir(path,'val',target_size=224)
+    # read_chinesefoodnet_from_xlsx(path)
+    # train_path = os.path.join(path,'train')
+    # create_VOC_data_txt(train_path,txt_path)
+    test_path = "D:\data"
+    # read_and_rewrite(text_path)
+    remove_useless_img(test_path,text_path)
 
--- visualize.py
+++ visualize.py
@@ -4,6 +4,9 @@ from PyQt5.QtWidgets import *
 from Relation_Learning_Network import test
 import imagenet_classes
 import numpy as np
+import util
+import matplotlib.pyplot as plt
+from mainWindows import *
 
 class MainForm(QWidget):
     def __init__(self, name='MainForm'):
@@ -25,6 +28,8 @@ class MainForm(QWidget):
         # 设置信号
         self.btn_chooseFile.clicked.connect(self.slot_btn_chooseFile)
 
+        self.path = "D:\BaiduNetdiskDownload\dataset_release/release_data"
+
 
     def slot_btn_chooseFile(self):
         fileName_choose, filetype = QFileDialog.getOpenFileName(self,
@@ -36,19 +41,21 @@ class MainForm(QWidget):
             print("取消选择")
             return
         else:
-            result = test(fileName_choose)
+            # result = test(fileName_choose)
+
+            _,labels,englishname = util.read_chinesefoodnet_from_xlsx(self.path)
             # 读取imagenet_classes.py文件下记录的标签对应英文名
-            labels = imagenet_classes.get_labels()
-            print(result)
+            # labels = imagenet_classes.get_labels()
+            # print(result)
 
-            prediction = labels[np.argmax(result)]
-            print(prediction)
-            res = QMessageBox.information(self,"预测结果",prediction, QMessageBox.Yes | QMessageBox.No)
-
-            if(QMessageBox.Yes == res):
-                print("[info] you clicked yes button!")
-            elif (QMessageBox.No == res):
-                print("[info] you clicked no button!")
+            # prediction = labels[np.argmax(result)][0]
+            # print(prediction)
+            # res = QMessageBox.information(self,"预测结果",prediction, QMessageBox.Yes | QMessageBox.No)
+            #
+            # if(QMessageBox.Yes == res):
+            #     print("[info] you clicked yes button!")
+            # elif (QMessageBox.No == res):
+            #     print("[info] you clicked no button!")
         # print("你选择的文件为:{}".format(fileName_choose))
 
 class MessageBox(QWidget):
@@ -59,8 +66,51 @@ class MessageBox(QWidget):
     def initUi(self):
         self.setWindowTitle()
 
+class MainWindow(QMainWindow,Ui_MainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.openButton.clicked.connect(self.slot_btn_chooseFile)
+
+        self.path = "D:\BaiduNetdiskDownload\dataset_release/release_data"
+
+    def slot_btn_chooseFile(self):
+        print("choose file...")
+        fileName_choose, filetype = QFileDialog.getOpenFileName(self,
+                                                                "选取文件",
+                                                                self.cwd,  # 起始路径
+                                                                "All Files (*);;Text Files (*.txt)")  # 设置文件扩展名过滤,用双分号间隔
+
+        if fileName_choose == "":
+            print("取消选择")
+            return
+        else:
+            jpg = QtGui.QPixmap(fileName_choose).scaled(self.showImage.width(),self.showImage.height())
+            self.showImage.setPixmap(jpg)
+
+            _,labels,englishname = util.read_chinesefoodnet_from_xlsx(self.path)
+
+            # result = test(fileName_choose)
+            # 读取imagenet_classes.py文件下记录的标签对应英文名
+            # labels = imagenet_classes.get_labels()
+            # print(result)
+
+            # prediction = labels[np.argmax(result)][0]
+            # print(prediction)
+            # res = QMessageBox.information(self,"预测结果",prediction, QMessageBox.Yes | QMessageBox.No)
+            #
+            # if(QMessageBox.Yes == res):
+            #     print("[info] you clicked yes button!")
+            # elif (QMessageBox.No == res):
+            #     print("[info] you clicked no button!")
+        # print("你选择的文件为:{}".format(fileName_choose))
+
+
 if __name__=="__main__":
     app = QApplication(sys.argv)
-    mainForm = MainForm('测试QFileDialog')
-    mainForm.show()
+    # mainForm = MainForm('测试QFileDialog')
+    # mainForm.show()
+    mainWindow = MainWindow()
+    mainWindow.show()
     sys.exit(app.exec_())
